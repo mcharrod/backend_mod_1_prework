87be27185318bff9616bee20bb8dc9ce4d515aba
Katy <m.catherine.harrod@gmail.com>
2021-09-17 13:43:43 -0600

    Add section3 assignments

---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,20 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print animals
+
+puts "the second animal animal is #{animals[1]}"
+puts "The third animal is #{animals[2]}"
+puts "the first animal is #{animals[0]}"
+puts "the fourth animal is #{animals[3]}"
+puts "the fifth animal is #{animals[4]}"
+puts "The third animal is #{animals[2]}"
+puts "The sixth animal is #{animals[5]}"
+puts "The fifth animal is #{animals[4]}"
+
+dishes = ['plate', 'bowl', 'cup', 'fork', 'knife', 'spoon']
+
+puts dishes[0]
+puts dishes[2]
+puts dishes[3]
+
+puts 'we now have enough dishes to serve lunch.'
