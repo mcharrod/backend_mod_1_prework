ac8355d5c9995a55ca619972438a9664ab3756dc
Katy <m.catherine.harrod@gmail.com>
2021-09-22 10:59:16 -0600

    Add work for burrito exercise

--- section4/exercises/burrito.rb
+++ section4/exercises/burrito.rb
@@ -1,4 +1,4 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
@@ -11,9 +11,15 @@ class Burrito
     @base     = base
     @toppings = toppings
   end
+
+  def protein=(brotein)
+    @protein = brotein
+  end
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
+puts "You hesitated, what do you want for your protein?"
+puts dinner.protein = "Chicken"
 p dinner.base
 p dinner.toppings
